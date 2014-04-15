b0e33d16df6ecd3d20583730e501e273945d1b34
Vytenis Bivainis <monrealis@gmail.com>
2014-04-15 23:09:38 +0300

    Add script to check Benford law.

---
+++ benford/benford.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+digits=$(seq 9)
+source=$(curl --silent http://www.delfi.lt)
+#source=$(curl --silent http://www.15min.lt)
+#source=$(curl --silent http://www.lrytas.lt)
+#source=$(curl --silent http://en.wikipedia.org/wiki/Lithuania)
+
+for digit in $digits ; do
+	array[$digit]=$(echo $source | fgrep -o $digit | wc -l)
+done
+
+sum=$(echo ${array[@]} | xargs -n 1 | paste -s -d + | bc)
+
+for digit in $digits ; do
+	count=${array[$digit]}
+	percent=$(echo "scale=50; 100 * $count / $sum" | bc)
+	printf "%s: %5.2f (%4d/%4d)\n" $digit $percent $count $sum
+done
