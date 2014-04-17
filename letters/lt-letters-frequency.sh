2fc5c98f2e28b0e212a05259dc7888de0670e295
Vytenis Bivainis <monrealis@gmail.com>
2014-04-17 21:18:00 +0300

    Change HTTP client from curl to wget in lt-letters-frequency.sh.

--- letters/lt-letters-frequency.sh
+++ letters/lt-letters-frequency.sh
@@ -2,8 +2,8 @@
 set -e
 uppercase=AĄBCČDEĘĖFGHIĮYJKLMNOPRSŠTUŲŪVZŽ
 lowercase=aąbcčdeęėfghiįyjklmnoprsštuųūvzž
-#text=$(curl --silent http://www.delfi.lt)
-text=$(curl --silent http://www.15min.lt)
+#text=$(wget -qO- http://www.delfi.lt)
+text=$(wget -qO- http://www.15min.lt)
 text=$(echo $text | tr $uppercase $lowercase | tr -cd $lowercase)
 
 total=$(echo -n $text | wc -m)
