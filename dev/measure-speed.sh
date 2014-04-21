566695c30e7ed1b54a21c63afcf263db34be04cd
Vytenis Bivainis <monrealis@gmail.com>
2014-04-21 19:31:08 +0300

    Add more precision to speed measurement.

--- dev/measure-speed.sh
+++ dev/measure-speed.sh
@@ -3,5 +3,5 @@ if=/dev/zero
 of=/dev/null
 $(which time) -f "%e\n" -o /tmp/duration.txt dd if=$if of=$of bs=1024 count=1048576 2> /dev/null
 seconds=$( cat /tmp/duration.txt )
-speed=$(echo 1024 / $seconds | bc)
+speed=$(echo "scale=2; 1024 / $seconds" | bc)
 echo $speed MB/s
