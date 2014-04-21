47ce6d2fc5af9cf0f8d4fe42029eba39e842e6d9
Vytenis Bivainis <monrealis@gmail.com>
2014-04-21 11:43:34 +0300

    Compare only files and sort before comparing.

--- diff-tools/compare-dirs.sh
+++ diff-tools/compare-dirs.sh
@@ -2,7 +2,7 @@
 left=.
 right=.
 cd $left
-find . -printf "%p %s\n" > /tmp/left.txt
+find . -type f -printf "%p %s\n" | sort > /tmp/left.txt
 cd $right
-find . -printf "%p %s\n" > /tmp/right.txt
+find . -type f -printf "%p %s\n" | sort > /tmp/right.txt
 kdiff3 /tmp/left.txt /tmp/right.txt
