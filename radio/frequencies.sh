cc3a84acf5b1a1039958ccfbbdfabd2bcfedc8e6
Vytenis Bivainis <monrealis@gmail.com>
2015-04-13 23:22:48 +0300

    Add some parsing.

--- radio/frequencies.sh
+++ radio/frequencies.sh
@@ -2,11 +2,12 @@
 url1=http://www.rtk.lt/lt/radijas_ir_televizija/radijo_programos/vilniuje_transliuojanciu_radijo_stociu_dazniai
 url2=http://www.rtk.lt/lt/radijas_ir_televizija/radijo_programos/kaune_transliuojanciu_radijo_stociu_dazniai
 urls=($url1 $url2)
-names=(vilnius kaunas)
+names=(vilnius.html kaunas.html)
 for index in 0 1; do
     url=${urls[index]}
-    file=${names[index]}.html
+    file=${names[index]}
     echo $url $file
-    curl -s $url -o $file
-    sleep 2
+    [ -e $file ] || ( curl -s $url -o $file ; sleep 2)
 done
+
+sed -n -r '/414px/,/<[/]table>/ p' kaunas.html
