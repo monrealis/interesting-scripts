7258e89f7459718bb89efd96aeb6d8704b9cb641
Vytenis Bivainis <monrealis@gmail.com>
2015-04-16 23:06:23 +0300

    Add remove-ns.xsl.

--- radio/frequencies.sh
+++ radio/frequencies.sh
@@ -6,10 +6,10 @@ names=(vilnius.html kaunas.html)
 for index in 0 1; do
     url=${urls[index]}
     file=${names[index]}
-    echo $url $file
+    # echo $url $file
     [ -e $file ] || ( curl -s $url -o $file ; sleep 2)
 done
-
 sed -n -r '/414px/,/<[/]table>/ p' kaunas.html \
 | tidy  --doctype omit -asxml -numeric -utf8 -f /dev/null \
+| xmlstarlet tr remove-ns.xsl \
 | xmlstarlet tr table.xsl
