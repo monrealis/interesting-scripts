b064d6022e40bf0b8ba40f39c24abeeda0a697a9
Vytenis Bivainis <monrealis@gmail.com>
2016-10-03 00:23:58 +0300

    Add more tagging commands.

--- mp3/update-id3-fields.sh
+++ mp3/update-id3-fields.sh
@@ -3,7 +3,11 @@
 # make track numbers equal
 id3v2 -T 0 *.mp3
  
-# update title from file name
+# update title from file name. Does not work?
 find -name "*.mp3" -print0 | xargs -r0i echo id3v2 -t {} {}
 
- 
+# Renumber track numbers
+ls *.mp3 | awk '{print "id3v2", "-T", NR, "\"" $0 "\""}' | bash
+
+# update title from file name. Does not work?
+ls *.mp3 | awk '{print "id3v2", "-t", "\"" $0 "\"", "\"" $0 "\""}'
