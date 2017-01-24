fbf2cfd31e68885ffa12d65060c77e92b655b596
Vytenis Bivainis <monrealis@gmail.com>
2017-01-24 23:07:36 +0200

    Add key parameter.

--- ciphers/blowfish.sh
+++ ciphers/blowfish.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-PASSWORD=z
+PASSWORD=
 echo Using password "$PASSWORD"
 
 echo Labas > in.txt
 cat in.txt
 
-openssl enc -bf -nosalt -in in.txt -out out.txt -pass pass:$PASSWORD
+openssl enc -bf -nosalt -in in.txt -out out.txt -pass pass:$PASSWORD -K 0
 cat out.txt
 echo
 
-openssl enc -bf -d -nosalt -in out.txt -out in2.txt -pass pass:$PASSWORD
+openssl enc -bf -d -nosalt -in out.txt -out in2.txt -pass pass:$PASSWORD -K 0
 cat in2.txt
