a7a8d1f9029d1aa3097901c8644eddc3e9ef887b
Vytenis Bivainis <monrealis@gmail.com>
2018-08-22 00:30:42 +0300

    Rename variables

--- images/adform/compose.sh
+++ images/adform/compose.sh
@@ -4,23 +4,20 @@
 # https://www.imagemagick.org/discourse-server/viewtopic.php?t=17441
 
 
-left=22
-top=40
-scale=65
+left=30
+top=38
+scale=62
 img=vytenis.gif
 
 width=`convert $img -format "%w" info:`
 height=`convert $img -format "%h" info:`
-xoff=`convert xc: -format "%[fx:$width*$left/100]" info:`
-yoff=`convert xc: -format "%[fx:$height*$top/100]" info:`
-
-wp=`convert xc: -format "%[fx:100-$top-$top]" info:`
-hp=`convert xc: -format "%[fx:100-$left-$left]" info:`
+xoffset=`convert xc: -format "%[fx:$width*$left/100]" info:`
+yoffset=`convert xc: -format "%[fx:$height*$top/100]" info:`
 
 from=$img
-for a in 1 2 3 4 ; do
+for a in 1 2 3 4 5 6 7 8 9 10 11 12 13 ; do
   to=$img-$a.gif
-  convert $from $from -geometry  %${scale}x${scale}+$xoff+$yoff -composite $to
+  convert $from $from -geometry  %${scale}x${scale}+$xoffset+$yoffset -composite $to
   from=$to
 done
 
