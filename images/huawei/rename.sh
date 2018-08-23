9229d996aa82ff6b617b8e0fc0d38dd941f97359
Vytenis Bivainis <monrealis@gmail.com>
2018-08-24 00:25:42 +0300

    Transform *.jpg files

--- images/huawei/rename.sh
+++ images/huawei/rename.sh
@@ -3,4 +3,4 @@
 # NAME=$(identify -format "IMG_%[EXIF:DateTime].jpg" 1.jpg | tr -d ':' | tr ' ' '_')
 # TODO
 
-exiftool "-FileName<CreateDate" -d "IMG_%Y%m%d_%H%M%S.jpg"
+exiftool "-FileName<CreateDate" -d "IMG_%Y%m%d_%H%M%S.jpg" *.jpg
