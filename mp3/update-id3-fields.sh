0383ae653c37868d3dc71ed16ac1f923d815a1ac
Vytenis Bivainis <monrealis@gmail.com>
2018-12-27 04:00:19 +0200

    Use "id3tag -1" instead of "id3v2"
    To avoid giberish

--- mp3/update-id3-fields.sh
+++ mp3/update-id3-fields.sh
@@ -8,8 +8,8 @@ find -name "*.mp3" -print0 | xargs -r0i echo id3v2 -t {} {}
 
 # Renumber track numbers
 # https://forums.sandisk.com/t5/SanDisk-Clip-Jam/Complaint-about-sorting-of-tracks-in-Sandisk-Clip-Jam/m-p/356399/highlight/true#M502
-ls *.mp3 | sort | awk '{print "id3v2", "-T", NR, "\"" $0 "\""}' | bash
-find -type f -name "*.mp3" | sort | awk '{print "id3v2", "-T", NR, "\"" $0 "\""}' | bash
+ls *.mp3 | sort | awk '{print "id3tag -1", "-T", NR, "\"" $0 "\""}' | bash
+find -type f -name "*.mp3" | sort | awk '{print "id3tag -1", "-T", NR, "\"" $0 "\""}' | bash
 
 # update title from file name. Does not work?
-ls *.mp3 | awk '{print "id3v2", "-t", "\"" $0 "\"", "\"" $0 "\""}'
+ls *.mp3 | awk '{print "id3tag -1", "-t", "\"" $0 "\"", "\"" $0 "\""}'
